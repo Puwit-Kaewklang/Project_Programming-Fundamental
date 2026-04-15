ea1c99936fa14d697a8cea010ef4e35eab08b86b
Puwit Kaewklang <pato.puwit@gmail.com>
2026-04-15 20:26:51 +0700

    Upload first version of project

---
+++ Project_Funda_API.py
@@ -0,0 +1,87 @@
+import pymysql
+import os
+from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI , HTTPException , Request ,Depends
+from pydantic import BaseModel
+import requests
+import json
+from sqlalchemy import create_engine , text
+from sqlalchemy.orm import sessionmaker , Session
+
+load_dotenv()
+job = FastAPI()
+job.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+engine = create_engine(
+    f"mysql+pymysql://{os.getenv('DB_USER')}:{os.getenv('DB_PASS')}@{os.getenv('DB_HOST')}/{os.getenv('DB_NAME')}" ,
+    pool_size=10 ,
+    max_overflow=20
+)
+
+SessionLocal = sessionmaker(bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
+@job.get("/All-Customer-Data")
+def all_customer_data(db: Session = Depends(get_db)):
+    try:
+        sql = text("""SELECT * FROM customer_detail""")
+        result = db.execute(sql)
+    except Exception as e:
+        raise HTTPException(status_code=500 , detail=str(e))
+    return result.mappings().all()
+
+@job.get("/All-Freelance-Data")
+def all_customer_data(db: Session = Depends(get_db)):
+    try:
+        sql = text("""SELECT * FROM freelance_detail""")
+        result = db.execute(sql)
+    except Exception as e:
+        raise HTTPException(status_code=500 , detail=str(e))
+    return result.mappings().all()
+
+@job.get("/Job-List")
+def job_list(db: Session = Depends(get_db)):
+    try:
+        sql = text("""SELECT j.Job_id , j.Job_Name , j.Posted_On , d.Budget , jt.Type_Of_Job , s.Status_Of_Job 
+                   FROM job as j inner join status as s inner join job_type as jt inner join detail as d
+                   ON j.Status_id = s.Status_id and j.Type_id = jt.Type_id and j.Job_id = d.Job_id""")
+        result = db.execute(sql)
+    except Exception as e:
+        raise HTTPException(status_code=500 , detail=str(e))
+    return result.mappings().all()
+
+@job.get("/Ranking-Freelance")
+def job_analytic(db: Session = Depends(get_db)):
+    try:
+        sql = text("""select f.Freelance_id , f.Freelance_FirstName , f.Freelance_LastName , count(j.Status_id) as Total_Completed_Job
+                    from freelance_detail as f inner join job_select as js inner join job as j inner join status as s
+                    on f.Freelance_id = js.Freelance_id and js.Job_id = j.Job_id and j.Status_id = s.Status_id and s.Status_id = 'S3'
+                    group by f.Freelance_id order by count(j.Status_id) desc;""")
+        result = db.execute(sql)
+    except Exception as e:
+        raise HTTPException(status_code=400 , detail=str(e))
+    
+    return result.mappings().all()
+
+@job.get("/Job-Analytic")
+def job_analytic(db: Session = Depends(get_db)):
+    try:
+        sql = text("""select jt.Type_Of_Job , count(j.Job_id) as Total_Job , sum(d.Budget) as Total_Budget 
+                    from job.job_type as jt inner join job.job as j inner join job.detail as d
+                    on	jt.Type_id = j.Type_id and j.Job_id = d.Job_id
+                    group by j.Type_id;""")
+        result = db.execute(sql)
+    except Exception as e:
+        raise HTTPException(status_code=400 , detail=str(e))
+    return result.mappings().all()
